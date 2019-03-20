2c3291ab98f6b479f3f947bcc17adc162ef4347d
Elliott Simmonds <4simme28@solent.ac.uk>
2019-03-20 11:13:56 +0000

    Created first stage of the simulation

--- src/transformersimulation/TransformerSimulation.java
+++ src/transformersimulation/TransformerSimulation.java
@@ -7,8 +7,11 @@ package transformersimulation;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
 import prins.simulator.Simulator;
+import prins.simulator.model.Location;
 import prins.simulator.view.Gui;
+import transformersimulation.model.Autobot;
 import transformersimulation.model.Planet;
 
 /**
@@ -19,16 +22,28 @@ public class TransformerSimulation extends Simulator {
 
     private Planet cybertron;
     private Gui gui;
+    private List<Autobot> autobots;
+    Resource resources;
     
     public TransformerSimulation() {
         cybertron = new Planet();
         gui = new Gui(cybertron);
+        gui.registerAgentColors(Autobot.class, Color.blue);
+        gui.registerAgentColors(Resource.class, Color.orange);
         
         populate();
     }
     
     private void populate() {
-
+        resources = new Resource(new Location(15,2));
+        cybertron.setAgent(resources, resources.getLocation());
+        
+        autobots = new ArrayList<>();
+        for (int i=0; i < TransformerConfig.MAX_TRANSFORMERS; i++) {
+            Autobot autobot = new Autobot(new Location(5,15));
+            autobot.generatePath(cybertron);
+            autobots.add(autobot);
+        }
     }
     
     @Override
@@ -49,7 +64,25 @@ public class TransformerSimulation extends Simulator {
 
     @Override
     protected void update() {
-
+        if (step % TransformerConfig.MAX_PATH == 0) {
+            //work out fitness
+            autobots.forEach(autobot -> {
+            
+            int rocketX = autobot.getLocation().getX();
+            int rocketY = autobot.getLocation().getY();
+            int targetX = resources.getLocation().getX();
+            int targetY = resources.getLocation().getY();
+            
+            double differenceX = Math.abs(targetX - rocketX);
+            double differenceY = Math.abs(targetY - rocketY);
+            double difference = Math.sqrt(Math.pow(differenceX,2) + Math.pow(differenceY,2));
+            double fitness = 1 / difference;
+            
+            autobot.setFitness(fitness);
+            
+            });
+        }
+        autobots.forEach(autobot -> autobot.act(cybertron));
     }
 
 }
---
+++ src/transformersimulation/model/Autobot.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transformersimulation.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import prins.simulator.model.Agent;
+import prins.simulator.model.Location;
+import transformersimulation.TransformerConfig;
+
+/**
+ *
+ * @author 4simme28
+ */
+public class Autobot extends Agent {
+        
+    private List<Location> path;
+    private double fitness;
+    
+    public Autobot(Location location) {
+        super(location);
+        path = new ArrayList<>();
+    }
+    
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+    
+    public double getFitness() {
+        return fitness;
+    }
+    
+    public void generatePath(Planet cybertron) {
+        path.clear();
+        path.add(location);
+        for (int i = 0; i < TransformerConfig.MAX_PATH; i++) {
+            Location lastLocation = path.get(path.size() -1);
+            List<Location> adjacentLocations = cybertron.findAdjacentLocations(lastLocation);
+            int nextInt = TransformerConfig.random.nextInt(adjacentLocations.size());
+            path.add(adjacentLocations.get(nextInt));
+        }
+    }
+    
+    public void setPath(List<Location> inputPath) {
+        path = inputPath;
+    }
+    
+    public List<Location> getPath() {
+        return path;
+    }
+    
+    public void act(Planet cybertron) {
+        Location nextLocation = location;
+        
+        // stops at end of list
+        if (path.indexOf(location) + 1 < path.size()) {
+            nextLocation = path.get(path.indexOf(location)+1);
+        }
+        
+        cybertron.setAgent(null, location);
+        cybertron.setAgent(this, nextLocation);
+        
+        location = nextLocation;
+    }
+    
+}
